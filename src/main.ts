59882303a925c4b88505dfa32d931aee5af103a8
IrinaRyazanskaya <ryazanskaya.irina@yandex.com>
2025-11-02 19:55:19 +0000

    Исправила передачу from и to

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,21 @@ import nodemailer from "nodemailer";
 import { createApplication } from "./application.js";
 import { apiConfig, mailConfig, serverConfig } from "./config.js";
 
-const mailTransporter = nodemailer.createTransport({
-  host: mailConfig.host,
-  port: mailConfig.port,
-  secure: true,
-  auth: {
-    user: mailConfig.username,
-    pass: mailConfig.password,
+const mailTransporter = nodemailer.createTransport(
+  {
+    host: mailConfig.host,
+    port: mailConfig.port,
+    secure: true,
+    auth: {
+      user: mailConfig.username,
+      pass: mailConfig.password,
+    },
   },
-  from: mailConfig.sender,
-  to: mailConfig.recipients,
-});
+  {
+    from: mailConfig.sender,
+    to: mailConfig.recipients,
+  },
+);
 
 const application = createApplication({
   authToken: apiConfig.token,
